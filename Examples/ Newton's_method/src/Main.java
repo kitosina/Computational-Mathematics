25bc19f01534cd5f035e66aaa8913c0f70cdc6f3
kitosina <kitosinaa555@mail.ru>
2020-10-10 16:47:33 +0700

    Final main

--- Examples/ Newton's_method/src/Main.java
+++ Examples/ Newton's_method/src/Main.java
@@ -3,56 +3,20 @@ public class Main {
         final Matrix matrix = new Matrix();
         final Functions functions = new Functions();
 
-        final Double EPS = 0.91;
-        final Double[][] matrixJacobian = new Double[2][2];
-        Double[][] matrixInversion = null;
+        final Double EPS = 1e-5;
+        Double[][] matrixJacobian = new Double[2][2];
+        Double[][] matrixInversion;
         Double[] resultFunction = new Double[2];
-        Double[] multiplyMatrix = new Double[2];
-
-        Double approximationX1 = 2.6;
-//        Double approximationX2 = 8.0;
+        Double[] multiplyMatrix;
+//        Double approximationX1 = -0.7;
+//        Double approximationY1 = 0.7;
+        Double approximationX1 = 2.8;
         Double approximationY1 = 1.0;
-//        Double approximationY2 = 2.0;
 
 
-
-
-        for(Integer i = 0; i < 2; i++) {
-            for (Integer j = 0; j < 2; j++) {
-                if (i == 0 && j == 1) {
-                    matrixJacobian[i][j] = matrix.getDerivative(approximationY1, i, j);
-                } else matrixJacobian[i][j] = matrix.getDerivative(approximationX1, i, j);
-            }
-        }
-
-        matrixInversion = matrix.getInversion(matrixJacobian);
-
-//        for(Integer i = 0; i < 2; i++) {
-//            for (Integer j = 0; j < 2; j++) {
-//                System.out.println(Float.parseFloat(String.valueOf(matrixJacobian[i][j])) + "\n");
-//                System.out.println(Float.parseFloat(String.valueOf(matrixInversion[i][j])) + "\n");
-//            }
-//        }
-
-        resultFunction[0] = functions.function1(approximationX1, approximationY1);
-        resultFunction[1] = functions.function2(approximationX1, approximationY1);
-
-        for(;;) {
-            multiplyMatrix = matrix.getMultiplyMatrix(resultFunction, matrixInversion);
-            if (multiplyMatrix[0] > multiplyMatrix[1]) {
-                if(Math.abs(multiplyMatrix[0]) < EPS) break;
-            } else {
-                if (Math.abs(multiplyMatrix[1]) < EPS) break;
-            }
-            resultFunction[0] = approximationX1 - multiplyMatrix[0];
-            resultFunction[1] = approximationY1 - multiplyMatrix[1];
-            approximationX1 = functions.function1(resultFunction[0], resultFunction[1]);
-            approximationY1 = functions.function2(resultFunction[0], resultFunction[1]);
-            resultFunction[0] = approximationX1;
-            resultFunction[1] = approximationY1;
-
-            for(Integer i = 0; i < 2; i++) {
-                for (Integer j = 0; j < 2; j++) {
+        do {
+            for(int i = 0; i < 2; i++) {
+                for (int j = 0; j < 2; j++) {
                     if (i == 0 && j == 1) {
                         matrixJacobian[i][j] = matrix.getDerivative(approximationY1, i, j);
                     } else matrixJacobian[i][j] = matrix.getDerivative(approximationX1, i, j);
@@ -60,11 +24,18 @@ public class Main {
             }
 
             matrixInversion = matrix.getInversion(matrixJacobian);
-//
-        }
-//
-        System.out.println(Float.parseFloat(String.valueOf(multiplyMatrix[0])));
-        System.out.println(Float.parseFloat(String.valueOf(multiplyMatrix[1])));
 
+            resultFunction[0] = functions.function1(approximationX1, approximationY1);
+            resultFunction[1] = functions.function2(approximationX1, approximationY1);
+
+            multiplyMatrix = matrix.getMultiplyMatrix(resultFunction, matrixInversion);
+
+            approximationX1 -= multiplyMatrix[0];
+            approximationY1 -= multiplyMatrix[1];
+        } while (Math.abs(multiplyMatrix[0]) < EPS && Math.abs(multiplyMatrix[1]) < EPS);
+
+        System.out.println("X = " + approximationX1);
+        System.out.println("Y = " + approximationY1);
     }
+
 }
